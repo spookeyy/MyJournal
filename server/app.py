77341fde5fab297ee7d6e4f6a8c186b5ddba4eeb
spookeyy <pangasmeshack@gmail.com>
2024-09-05 02:17:43 +0300

    main server file

--- server/app.py
+++ server/app.py
@@ -1,18 +1,7 @@
-import os
-from flask import Flask
-from flask_cors import CORS
-from flask_restful import Api
-from flask_jwt_extended import JWTManager
-from flask_migrate import Migrate
-from flask_bcrypt import Bcrypt
-from flask_sqlalchemy import SQLAlchemy
+from server import create_app
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-CORS(app)
-api = Api(app)
-jwt = JWTManager(app)
-bcrypt = Bcrypt(app)
-migrate = Migrate(app, db)
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
