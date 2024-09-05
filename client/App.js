43a3820064d7df5402452ddaff90caa68993de76
spookeyy <pangasmeshack@gmail.com>
2024-09-06 01:34:47 +0300

    main

--- client/App.js
+++ client/App.js
@@ -1,19 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { AuthProvider } from './app/context/AuthContext';
 import { StyleSheet, Text, View } from 'react-native';
+import LoginScreen from './app/screens/LoginScreen';
 
 export default function App() {
   return (
+    <AuthProvider>
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <LoginScreen />
       <StatusBar style="auto" />
     </View>
+    </AuthProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#blue',
     alignItems: 'center',
     justifyContent: 'center',
   },
