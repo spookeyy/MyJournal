c1365dd40d0ba3e12188b224ecfdea99f55f1f41
spookeyy <pangasmeshack@gmail.com>
2024-09-07 02:59:16 +0300

    client side

--- client/App.js
+++ client/App.js
@@ -19,7 +19,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#blue',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
