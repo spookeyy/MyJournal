93520f8c772a81c5a08d0b9bec06819570a026e5
spookeyy <pangasmeshack@gmail.com>
2024-10-12 23:30:56 +0300

    to be modified

--- client/app/screens/HomeScreen.jsx
+++ client/app/screens/HomeScreen.jsx
@@ -1,30 +1,26 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 import { useAuth } from "../context/AuthContext";
 import { useNavigation } from "@react-navigation/native";
-import EntryListScreen from "./EntryListScreen";
-// import { useToast } from "react-native-toast-notifications";
-// import EntryDetailScreen from "./EntryDetailScreen";
-import ProfileScreen from "./ProfileScreen";
-
 
 export default function HomeScreen() {
-    const { user } = useAuth();
-    const navigation = useNavigation();
-    // const toast = useToast();
+  const { currentUser } = useAuth();
+  const navigation = useNavigation();
 
-    return (
-        <View className="flex-1 justify-center p-5">
-            <Text className="text-lg font-bold">Welcome, {user.username}!</Text>
-            <Text className="text-lg">Your email is: {user.email}</Text>
-            <Button
-                title="View Entries"
-                onPress={() => navigation.navigate("Entries")}
-            />
-            <Button
-                title="View Profile"
-                onPress={() => navigation.navigate("Profile")}
-            />
-        </View>
-    );
+  return (
+    <View className="flex-1 justify-center p-5">
+      <Text className="text-lg font-bold">
+        Welcome, {currentUser.username}!
+      </Text>
+      <Text className="text-lg">Your email is: {currentUser.email}</Text>
+      <Button
+        title="View Entries"
+        onPress={() => navigation.navigate("JournalTab", { screen: "Entries" })}
+      />
+      <Button
+        title="View Profile"
+        onPress={() => navigation.navigate("ProfileTab")}
+      />
+    </View>
+  );
 }
